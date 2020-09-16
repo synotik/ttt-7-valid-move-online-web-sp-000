a1b0cf30add008bcd979fd7aeb7c12587916197a
Jeff Johnson <synotik@hotmail.com>
2020-09-16 07:27:18 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,18 @@
-# code your #valid_move? method here
+def invalid_move?(board, index)
+  if index < 0
+    return true
+  elsif index > 8
+    return true
+  elsif position_taken?
+    return true
+  else
+    return false
+end
 
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
